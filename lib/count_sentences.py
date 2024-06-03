3c7678be6e077221e6175a47da70752a2893709b
Robert Bubnik <rbubnik@gmail.com>
2024-06-03 16:43:56 -0400

    passed all tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,57 @@
 #!/usr/bin/env python3
-
+import re 
 class MyString:
-  pass
+  def __init__ (self,  value = ''):
+      self.value = value
+      
+    
+  
+  @property
+  def value(self):
+    return self._value 
+
+  @value.setter
+  def value(self, value):
+    if type(value) == str :
+      self._value = value
+    else:
+      print ("The value must be a string.")
+    
+
+
+
+
+  def is_sentence(self):
+    value = self.value 
+    if value[-1] == '.':
+      return True
+    else:
+      return False
+    
+  def is_question(self):
+    value = self.value
+    if value[-1] == '?':
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    value = self.value
+    if value[-1] == '!':
+        return True
+    else:
+        return False
+    
+  def count_sentences(self):
+    value = self.value
+    if not value:
+      return 0
+    
+    total = re.split(r'[.!?]\s+', value)
+
+    print('This is the length', (total))
+    return len(total)
+  
+    
+    
+    
